6f23f367a326bd636ee8440b8c05fdbc7ecebea0
Кирилл Хорев <khorev.spb@gmail.com>
2022-04-08 15:44:50 +0300

    Converter Page

--- pages/converter/currency-row.tsx
+++ pages/converter/currency-row.tsx
@@ -1,15 +1,15 @@
 const CurrencyRow = (props: any) => {
     const { currencyOptions, selectedCurrency, onChangeCurrency, } = props;
     return (
-        <div className='currencySelect currencySelectWrapper' onChange={onChangeCurrency}>
-            <select className="form-select" aria-label="Default select example" value={selectedCurrency}>
-                {
-                    currencyOptions.map((i: any) => {
-                        return <option value={i.values.USD.price} key={i.id}>{i.name}</option>
-                    })
-                }
-            </select>
-        </div>
+        // <div className='currencySelect currencySelectWrapper' onChange={onChangeCurrency}>
+        <select className="form-select" aria-label="Default select example" onChange={onChangeCurrency} value={selectedCurrency}>
+            {
+                currencyOptions.map((i: any) => {
+                    return <option value={i.values.USD.price} key={i.id}>{i.name}</option>
+                })
+            }
+        </select>
+        // </div>
     )
 }
 
