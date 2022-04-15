663ec2f2e1a16ec3aff63f47722fdf7cb2b61ecf
Кирилл Хорев <khorev.spb@gmail.com>
2022-04-15 12:59:29 +0300

    added next/auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,8 +5,8 @@ export default NextAuth({
     // Configure one or more authentication providers
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: '10276809735bc5077f4b',
+            clientSecret: '4890bc1d231a24f458d1ccaa8f48d4137b132c50',
         }),
         // ...add more providers here
     ],
